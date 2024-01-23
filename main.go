de3ba779097b5425d32cb656aa8a70d8eb4813b1
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2024-01-23 16:15:29 +0200

    fix(h2): missing content-length header (#10)
    ### Summary
    
    This commit changes the code so that it always adds the content-length
    header to requests.
    
    Co-authored-by: Wangchong Zhou <fffonion@gmail.com>

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"bytes"
 
 	"golang.org/x/net/http2"
 )
@@ -26,8 +27,13 @@ func makeH2Request(
 		Timeout:   time.Duration(timeout) * time.Second,
 		Transport: tr,
 	}
-
-	req, err := http.NewRequest(method, url, requestBody)
+        // convert the buffer to a interface that supports `.Len()` so that Content-Length header is added
+	b, err := io.ReadAll(requestBody)
+	if err != nil {
+		return err
+	}
+	reqBody := bytes.NewBuffer(b)
+	req, err := http.NewRequest(method, url, reqBody)
 	if err != nil {
 		return err
 	}
